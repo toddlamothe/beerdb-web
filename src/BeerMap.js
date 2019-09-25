7a9342993b540690260196eea56a08891652be51
Todd LaMothe <toddlamothe@USMs-MacBook-Air-4.local>
2019-09-25 11:00:50 -0400

    Add react-google-map dependency and map component

---
+++ src/BeerMap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// class BeerMap extends React.Component {
+// 
+// }
+
+function BeerMap(props) {
+  return (
+    <div id="map" className="beerdb-map"></div>
+  )
+}
+
+export default BeerMap;
