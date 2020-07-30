7582843bf0efaa6671e7ddb8c470b5d484a38bef
Todd LaMothe <todd@Wills-MacBook-Pro.local>
2020-07-30 16:44:27 -0400

    Code cleanup

--- src/services/GeoService.js
+++ src/services/GeoService.js
@@ -4,11 +4,8 @@ class GeoService {
     if (!lat || !lng) return "Invalid lat/lng parameters";
 
     const geoCodeBaseUrl = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB8XchO4u2Ig273475Zl1RImvskWNZDEOw&latlng=" + lat + "," + lng;
-    // let myHeaders = new Headers();
-    // myHeaders.append('Content-Type', 'image/jpeg');
     const initForGeoCodeApi = {
       method: 'GET',
-      // headers: myHeaders,
       mode: 'cors',
       cache: 'default'
     };
@@ -21,28 +18,7 @@ class GeoService {
     })
       .then(res => res.json())
       .then((data) => {
-        // console.log("geoCode data = ", data);
         callback(data);
-        // if (data.totalResults && data.totalResults > 0) {
-        //   breweries = data.data.map((brewery) => {
-        //       return {
-        //           "name" : brewery.brewery.name,
-        //           "id" : brewery.breweryId,
-        //           "description" : brewery.brewery.description,
-        //           "url" : brewery.brewery.website,
-        //           "coords" : {
-        //               "lat" : brewery.latitude,
-        //               "lng" : brewery.longitude
-        //           },
-        //           "images" : brewery.brewery.images
-        //       }
-        //   });
-        //   callback(breweries);
-        // }
-        // else {
-        //   console.log("search returned no results");
-        //   callback();
-        // }
       })
       .catch(console.log)
 
