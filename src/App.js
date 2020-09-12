cb28a6626cdc7cf5ac1eef8da8b90dc3972e99be
Todd LaMothe <todd@Todds-MacBook-Air.local>
2020-09-12 14:19:11 -0400

    Began adding routing and about page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Main from './components/Main';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-177244569-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
@@ -10,10 +10,17 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <div className="App">
-      <Route children={(props) => (
-        <Main {...props} />
-        )}
-      />
+      <Switch>
+        <Route path="/" children={(props) => (
+          <Main {...props} />
+          )}
+        />
+        <Route path="/about"
+          
+        />
+
+
+      </Switch>
     </div>
   );
 }
