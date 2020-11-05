cb89ba00c9548bd8fd26e126779344b0a0006f21
Todd LaMothe <todd@Todds-MacBook-Air.local>
2020-11-05 08:02:38 -0500

    Redirect URLs with trailing slashes by removing the slash

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import About from './components/About.js'
 import PageNotFound from './components/404.js'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect, useLocation} from 'react-router-dom';
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-177244569-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <div className="App">
       <Switch>
+        <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
         <Route exact path="/" children={(props) => (
           <Main {...props} />
           )}
